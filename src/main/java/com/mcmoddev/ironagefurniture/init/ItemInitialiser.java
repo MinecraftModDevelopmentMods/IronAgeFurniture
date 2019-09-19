7297048129d83f7b73d6801df2417aa383fa5aa4
SkyBlade1978 <johnbraham1978@outlook.com>
2019-09-19 14:17:07 +0100

    item inventory works now (thanks proxy)

--- src/main/java/com/mcmoddev/ironagefurniture/init/ItemInitialiser.java
+++ src/main/java/com/mcmoddev/ironagefurniture/init/ItemInitialiser.java
@@ -2,6 +2,8 @@ package com.mcmoddev.ironagefurniture.init;
 
 import com.mcmoddev.ironagefurniture.Ironagefurniture;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -12,4 +14,13 @@ public class ItemInitialiser {
 		item.setUnlocalizedName(Ironagefurniture.MODID + "." + name);
 		return item;
 	}
+	
+	public static void RegisterItemRenders() {
+
+		for(String name : Ironagefurniture.ItemRegistry.keySet()){
+			Item i = Ironagefurniture.ItemRegistry.get(name);
+    		Minecraft.getMinecraft().getRenderItem().getItemModelMesher()
+    				.register(i, 0, new ModelResourceLocation(Ironagefurniture.MODID + ":" + name, "inventory"));
+    	}
+    }
 }
