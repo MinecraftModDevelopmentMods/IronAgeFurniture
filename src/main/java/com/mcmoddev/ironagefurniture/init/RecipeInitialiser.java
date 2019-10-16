12dd408dc1eb6b34a68f3aed232a163ebf90306d
SkyBlade1978 <JohnBraham1978@Outlook.com>
2019-10-16 17:26:23 +0100

    natura recipe integration

--- src/main/java/com/mcmoddev/ironagefurniture/init/RecipeInitialiser.java
+++ src/main/java/com/mcmoddev/ironagefurniture/init/RecipeInitialiser.java
@@ -1,5 +1,8 @@
 package com.mcmoddev.ironagefurniture.init;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.mcmoddev.ironagefurniture.IronAgeFurnitureConfiguration;
 
 import net.minecraft.block.Block;
@@ -12,6 +15,8 @@ import net.minecraftforge.oredict.ShapedOreRecipe;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 public class RecipeInitialiser {
+	private static Logger logger;
+	
 	protected RecipeInitialiser() {
 		throw new IllegalAccessError("This class cannot be instansiated");
 	}
@@ -156,20 +161,86 @@ public class RecipeInitialiser {
 		}
 		
 		if (IronAgeFurnitureConfiguration.INTEGRATION_NATURA && Loader.isModLoaded("natura")) {
+			logger.info("natura recipe integration");
+			
+			ItemStack MAPLE = new ItemStack(Block.getBlockFromName("natura:overworld_planks"), 1, 0);
+			ItemStack SILVERBELL = new ItemStack(Block.getBlockFromName("natura:overworld_planks"), 1, 1);
+			ItemStack AMARANTH = new ItemStack(Block.getBlockFromName("natura:overworld_planks"), 1, 2);
+			ItemStack TIGER = new ItemStack(Block.getBlockFromName("natura:overworld_planks"), 1, 3);
+			ItemStack WILLOW = new ItemStack(Block.getBlockFromName("natura:overworld_planks"), 1, 4);
+			ItemStack EUCALYPTUS = new ItemStack(Block.getBlockFromName("natura:overworld_planks"), 1, 5);
+			ItemStack HOPSEED = new ItemStack(Block.getBlockFromName("natura:overworld_planks"), 1, 6);
+			ItemStack SAKURA = new ItemStack(Block.getBlockFromName("natura:overworld_planks"), 1, 7);
+			ItemStack REDWOOD = new ItemStack(Block.getBlockFromName("natura:overworld_planks"), 1, 8);
+			ItemStack GHOSTWOOD = new ItemStack(Block.getBlockFromName("natura:nether_planks"), 1, 0);
+			ItemStack BLOODWOOD = new ItemStack(Block.getBlockFromName("natura:nether_planks"), 1, 1);
+			ItemStack FUSEWOOD = new ItemStack(Block.getBlockFromName("natura:nether_planks"), 1, 3);
+			ItemStack DARKWOOD = new ItemStack(Block.getBlockFromName("natura:nether_planks"), 1, 2);
+			
+			logger.info("maple: " + MAPLE.getItem().getUnlocalizedName());
+			
 			if (IronAgeFurnitureConfiguration.GENERATE_CLASSIC_CHAIRS) {
-				
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_maple, 1), "x  ", "xxx", "y y", 'x', MAPLE, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_silverbell, 1), "x  ", "xxx", "y y", 'x', SILVERBELL, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_amaranth, 1), "x  ", "xxx", "y y", 'x', AMARANTH, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_tiger, 1), "x  ", "xxx", "y y", 'x', TIGER, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_willow, 1), "x  ", "xxx", "y y", 'x', WILLOW, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_eucalyptus, 1), "x  ", "xxx", "y y", 'x', EUCALYPTUS, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_hopseed, 1), "x  ", "xxx", "y y", 'x', HOPSEED , 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_sakura, 1), "x  ", "xxx", "y y", 'x', SAKURA, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_redwood, 1), "x  ", "xxx", "y y", 'x', REDWOOD, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_ghostwood, 1), "x  ", "xxx", "y y", 'x', GHOSTWOOD , 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_bloodwood, 1), "x  ", "xxx", "y y", 'x', BLOODWOOD , 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_fusewood, 1), "x  ", "xxx", "y y", 'x', FUSEWOOD, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_darkwood, 1), "x  ", "xxx", "y y", 'x', DARKWOOD, 'y', "stickWood"));
 			}
 			
 			if (IronAgeFurnitureConfiguration.GENERATE_SHIELD_CHAIRS) {
-				
+				GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_shield_natura_maple, 1), new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_maple,1), new ItemStack(Items.SHIELD,1)));
+				GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_shield_natura_silverbell, 1), new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_silverbell,1), new ItemStack(Items.SHIELD,1)));
+				GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_shield_natura_amaranth, 1), new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_amaranth,1), new ItemStack(Items.SHIELD,1)));
+				GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_shield_natura_tiger, 1), new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_tiger,1), new ItemStack(Items.SHIELD,1)));
+				GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_shield_natura_willow, 1), new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_willow,1), new ItemStack(Items.SHIELD,1)));
+				GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_shield_natura_eucalyptus, 1), new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_eucalyptus,1), new ItemStack(Items.SHIELD,1)));
+				GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_shield_natura_hopseed, 1), new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_hopseed,1), new ItemStack(Items.SHIELD,1)));
+				GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_shield_natura_sakura, 1), new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_sakura,1), new ItemStack(Items.SHIELD,1)));
+				GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_shield_natura_redwood, 1), new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_redwood,1), new ItemStack(Items.SHIELD,1)));
+				GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_shield_natura_ghostwood, 1), new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_ghostwood,1), new ItemStack(Items.SHIELD,1)));
+				GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_shield_natura_bloodwood, 1), new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_bloodwood,1), new ItemStack(Items.SHIELD,1)));
+				GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_shield_natura_fusewood, 1), new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_fusewood,1), new ItemStack(Items.SHIELD,1)));
+				GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_shield_natura_darkwood, 1), new ItemStack(BlockInitialiser.chair_wood_ironage_classic_natura_darkwood,1), new ItemStack(Items.SHIELD,1)));
 			}
 			
 			if (IronAgeFurnitureConfiguration.GENERATE_SHORT_STOOLS) {
-				
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_short_natura_maple, 1), " x ", "yyy", 'x', MAPLE, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_short_natura_silverbell, 1), " x ", "yyy", 'x', SILVERBELL, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_short_natura_amaranth, 1), " x ", "yyy", 'x', AMARANTH, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_short_natura_tiger, 1), " x ", "yyy", 'x', TIGER, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_short_natura_willow, 1), " x ", "yyy", 'x', WILLOW, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_short_natura_eucalyptus, 1), " x ", "yyy", 'x', EUCALYPTUS, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_short_natura_hopseed, 1), " x ", "yyy", 'x', HOPSEED, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_short_natura_sakura, 1), " x ", "yyy", 'x', SAKURA, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_short_natura_redwood, 1), " x ", "yyy", 'x', REDWOOD, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_short_natura_ghostwood, 1), " x ", "yyy", 'x', GHOSTWOOD , 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_short_natura_bloodwood, 1), " x ", "yyy", 'x', BLOODWOOD , 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_short_natura_fusewood, 1), " x ", "yyy", 'x', FUSEWOOD, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_short_natura_darkwood, 1), " x ", "yyy", 'x', DARKWOOD, 'y', "stickWood"));
 			}
 			
 			if (IronAgeFurnitureConfiguration.GENERATE_TALL_STOOLS) {
-				
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_tall_natura_maple, 1), " x ", "yyy", "yyy", 'x', MAPLE, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_tall_natura_silverbell, 1), " x ", "yyy", "yyy", 'x', SILVERBELL, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_tall_natura_amaranth, 1), " x ", "yyy", "yyy", 'x', AMARANTH, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_tall_natura_tiger, 1), " x ", "yyy", "yyy", 'x', TIGER, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_tall_natura_willow, 1), " x ", "yyy", "yyy", 'x', WILLOW, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_tall_natura_eucalyptus, 1), " x ", "yyy", "yyy", 'x', EUCALYPTUS, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_tall_natura_hopseed, 1), " x ", "yyy", "yyy", 'x', HOPSEED, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_tall_natura_sakura, 1), " x ", "yyy", "yyy", 'x', SAKURA, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_tall_natura_redwood, 1), " x ", "yyy", "yyy", 'x', REDWOOD, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_tall_natura_ghostwood, 1), " x ", "yyy", "yyy", 'x', GHOSTWOOD , 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_tall_natura_bloodwood, 1), " x ", "yyy", "yyy", 'x', BLOODWOOD , 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_tall_natura_fusewood, 1), " x ", "yyy", "yyy", 'x', FUSEWOOD, 'y', "stickWood"));
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockInitialiser.chair_wood_ironage_stool_tall_natura_darkwood, 1), " x ", "yyy", "yyy", 'x', DARKWOOD, 'y', "stickWood"));
 			}
 		}
 	}
