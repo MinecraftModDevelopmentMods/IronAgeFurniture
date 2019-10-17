1e390fc708d6b9179a67c471d944a2f4930c70cd
SkyBlade1978 <JohnBraham1978@Outlook.com>
2019-10-18 00:21:21 +0100

    tidied up code and made more like 1.14

---
+++ src/main/java/com/mcmoddev/ironagefurniture/BlockObjectHolder.java
@@ -0,0 +1,157 @@
+package com.mcmoddev.ironagefurniture;
+
+import net.minecraft.block.Block;
+
+public class BlockObjectHolder {
+	public static Block chair_wood_ironage_classic_oak;
+	public static Block chair_wood_ironage_classic_acacia;
+	public static Block chair_wood_ironage_classic_big_oak;
+	public static Block chair_wood_ironage_classic_birch;
+	public static Block chair_wood_ironage_classic_jungle;
+	public static Block chair_wood_ironage_classic_spruce;
+	
+	public static Block chair_wood_ironage_shield_oak;
+	public static Block chair_wood_ironage_shield_acacia;
+	public static Block chair_wood_ironage_shield_big_oak;
+	public static Block chair_wood_ironage_shield_birch;
+	public static Block chair_wood_ironage_shield_jungle;
+	public static Block chair_wood_ironage_shield_spruce;		
+	
+	public static Block chair_wood_ironage_stool_short_oak;
+	public static Block chair_wood_ironage_stool_short_acacia;
+	public static Block chair_wood_ironage_stool_short_big_oak;
+	public static Block chair_wood_ironage_stool_short_birch;
+	public static Block chair_wood_ironage_stool_short_jungle;
+	public static Block chair_wood_ironage_stool_short_spruce;
+	
+	public static Block chair_wood_ironage_stool_tall_acacia;
+	public static Block chair_wood_ironage_stool_tall_big_oak;
+	public static Block chair_wood_ironage_stool_tall_birch;
+	public static Block chair_wood_ironage_stool_tall_jungle;	
+	public static Block chair_wood_ironage_stool_tall_oak;
+	public static Block chair_wood_ironage_stool_tall_spruce;
+	
+	public static Block chair_wood_ironage_classic_biomesoplenty_cherry;
+	public static Block chair_wood_ironage_classic_biomesoplenty_ebony;
+	public static Block chair_wood_ironage_classic_biomesoplenty_ethereal;
+	public static Block chair_wood_ironage_classic_biomesoplenty_eucalyptus;
+	public static Block chair_wood_ironage_classic_biomesoplenty_fir;
+	public static Block chair_wood_ironage_classic_biomesoplenty_hellbark;
+	public static Block chair_wood_ironage_classic_biomesoplenty_jacaranda;
+	public static Block chair_wood_ironage_classic_biomesoplenty_magic;
+	public static Block chair_wood_ironage_classic_biomesoplenty_mahogany;
+	public static Block chair_wood_ironage_classic_biomesoplenty_mangrove;
+	public static Block chair_wood_ironage_classic_biomesoplenty_palm;
+	public static Block chair_wood_ironage_classic_biomesoplenty_pine;
+	public static Block chair_wood_ironage_classic_biomesoplenty_redwood; 
+	public static Block chair_wood_ironage_classic_biomesoplenty_sacred_oak;
+	public static Block chair_wood_ironage_classic_biomesoplenty_umbran;
+	public static Block chair_wood_ironage_classic_biomesoplenty_willow;
+	
+	public static Block chair_wood_ironage_shield_biomesoplenty_cherry;
+	public static Block chair_wood_ironage_shield_biomesoplenty_ebony;
+	public static Block chair_wood_ironage_shield_biomesoplenty_ethereal;
+	public static Block chair_wood_ironage_shield_biomesoplenty_eucalyptus;
+	public static Block chair_wood_ironage_shield_biomesoplenty_fir;
+	public static Block chair_wood_ironage_shield_biomesoplenty_hellbark;
+	public static Block chair_wood_ironage_shield_biomesoplenty_jacaranda;
+	public static Block chair_wood_ironage_shield_biomesoplenty_magic;
+	public static Block chair_wood_ironage_shield_biomesoplenty_mahogany;
+	public static Block chair_wood_ironage_shield_biomesoplenty_mangrove;
+	public static Block chair_wood_ironage_shield_biomesoplenty_palm;
+	public static Block chair_wood_ironage_shield_biomesoplenty_pine;
+	public static Block chair_wood_ironage_shield_biomesoplenty_redwood; 
+	public static Block chair_wood_ironage_shield_biomesoplenty_sacred_oak;
+	public static Block chair_wood_ironage_shield_biomesoplenty_umbran;
+	public static Block chair_wood_ironage_shield_biomesoplenty_willow;
+	
+	public static Block chair_wood_ironage_stool_short_biomesoplenty_cherry;
+	public static Block chair_wood_ironage_stool_short_biomesoplenty_ebony;
+	public static Block chair_wood_ironage_stool_short_biomesoplenty_ethereal;
+	public static Block chair_wood_ironage_stool_short_biomesoplenty_eucalyptus;
+	public static Block chair_wood_ironage_stool_short_biomesoplenty_fir;
+	public static Block chair_wood_ironage_stool_short_biomesoplenty_hellbark;
+	public static Block chair_wood_ironage_stool_short_biomesoplenty_jacaranda;
+	public static Block chair_wood_ironage_stool_short_biomesoplenty_magic;
+	public static Block chair_wood_ironage_stool_short_biomesoplenty_mahogany;
+	public static Block chair_wood_ironage_stool_short_biomesoplenty_mangrove;
+	public static Block chair_wood_ironage_stool_short_biomesoplenty_palm;
+	public static Block chair_wood_ironage_stool_short_biomesoplenty_pine;
+	public static Block chair_wood_ironage_stool_short_biomesoplenty_redwood;
+	public static Block chair_wood_ironage_stool_short_biomesoplenty_sacred_oak;
+	public static Block chair_wood_ironage_stool_short_biomesoplenty_umbran;
+	public static Block chair_wood_ironage_stool_short_biomesoplenty_willow;
+
+	public static Block chair_wood_ironage_stool_tall_biomesoplenty_cherry;
+	public static Block chair_wood_ironage_stool_tall_biomesoplenty_ebony;
+	public static Block chair_wood_ironage_stool_tall_biomesoplenty_ethereal;
+	public static Block chair_wood_ironage_stool_tall_biomesoplenty_eucalyptus;
+	public static Block chair_wood_ironage_stool_tall_biomesoplenty_fir;
+	public static Block chair_wood_ironage_stool_tall_biomesoplenty_hellbark;
+	public static Block chair_wood_ironage_stool_tall_biomesoplenty_jacaranda;
+	public static Block chair_wood_ironage_stool_tall_biomesoplenty_magic;
+	public static Block chair_wood_ironage_stool_tall_biomesoplenty_mahogany;
+	public static Block chair_wood_ironage_stool_tall_biomesoplenty_mangrove;
+	public static Block chair_wood_ironage_stool_tall_biomesoplenty_palm;
+	public static Block chair_wood_ironage_stool_tall_biomesoplenty_pine;
+	public static Block chair_wood_ironage_stool_tall_biomesoplenty_redwood;
+	public static Block chair_wood_ironage_stool_tall_biomesoplenty_sacred_oak;
+	public static Block chair_wood_ironage_stool_tall_biomesoplenty_umbran;
+	public static Block chair_wood_ironage_stool_tall_biomesoplenty_willow;
+	
+	public static Block chair_wood_ironage_classic_natura_amaranth;
+	public static Block chair_wood_ironage_classic_natura_eucalyptus;
+	public static Block chair_wood_ironage_classic_natura_hopseed;
+	public static Block chair_wood_ironage_classic_natura_maple;
+	public static Block chair_wood_ironage_classic_natura_redwood; 
+	public static Block chair_wood_ironage_classic_natura_sakura;
+	public static Block chair_wood_ironage_classic_natura_silverbell;
+	public static Block chair_wood_ironage_classic_natura_tiger;
+	public static Block chair_wood_ironage_classic_natura_willow;
+	public static Block chair_wood_ironage_classic_natura_bloodwood;
+	public static Block chair_wood_ironage_classic_natura_darkwood;
+	public static Block chair_wood_ironage_classic_natura_fusewood;
+	public static Block chair_wood_ironage_classic_natura_ghostwood;
+	
+	public static Block chair_wood_ironage_shield_natura_amaranth;
+	public static Block chair_wood_ironage_shield_natura_eucalyptus;
+	public static Block chair_wood_ironage_shield_natura_hopseed;
+	public static Block chair_wood_ironage_shield_natura_maple;
+	public static Block chair_wood_ironage_shield_natura_redwood; 
+	public static Block chair_wood_ironage_shield_natura_sakura;
+	public static Block chair_wood_ironage_shield_natura_silverbell;
+	public static Block chair_wood_ironage_shield_natura_tiger;
+	public static Block chair_wood_ironage_shield_natura_willow;
+	public static Block chair_wood_ironage_shield_natura_bloodwood;
+	public static Block chair_wood_ironage_shield_natura_darkwood;
+	public static Block chair_wood_ironage_shield_natura_fusewood;
+	public static Block chair_wood_ironage_shield_natura_ghostwood;
+	
+	public static Block chair_wood_ironage_stool_short_natura_amaranth;
+	public static Block chair_wood_ironage_stool_short_natura_eucalyptus;
+	public static Block chair_wood_ironage_stool_short_natura_hopseed;
+	public static Block chair_wood_ironage_stool_short_natura_maple;
+	public static Block chair_wood_ironage_stool_short_natura_redwood; 
+	public static Block chair_wood_ironage_stool_short_natura_sakura;
+	public static Block chair_wood_ironage_stool_short_natura_silverbell;
+	public static Block chair_wood_ironage_stool_short_natura_tiger;
+	public static Block chair_wood_ironage_stool_short_natura_willow;
+	public static Block chair_wood_ironage_stool_short_natura_bloodwood;
+	public static Block chair_wood_ironage_stool_short_natura_darkwood;
+	public static Block chair_wood_ironage_stool_short_natura_fusewood;
+	public static Block chair_wood_ironage_stool_short_natura_ghostwood;
+	
+	public static Block chair_wood_ironage_stool_tall_natura_amaranth;
+	public static Block chair_wood_ironage_stool_tall_natura_eucalyptus;
+	public static Block chair_wood_ironage_stool_tall_natura_hopseed; 
+	public static Block chair_wood_ironage_stool_tall_natura_maple;
+	public static Block chair_wood_ironage_stool_tall_natura_redwood; 
+	public static Block chair_wood_ironage_stool_tall_natura_sakura;
+	public static Block chair_wood_ironage_stool_tall_natura_silverbell;
+	public static Block chair_wood_ironage_stool_tall_natura_tiger;
+	public static Block chair_wood_ironage_stool_tall_natura_willow;
+	public static Block chair_wood_ironage_stool_tall_natura_bloodwood;
+	public static Block chair_wood_ironage_stool_tall_natura_darkwood;
+	public static Block chair_wood_ironage_stool_tall_natura_fusewood;
+	public static Block chair_wood_ironage_stool_tall_natura_ghostwood;
+}
