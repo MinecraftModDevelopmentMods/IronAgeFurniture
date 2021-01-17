b4ec9e7cd2c0096f3f6f4c5b5068c99cda9f8082
SkyBlade1978 <johnbraham1978@outlook.com>
2021-01-17 12:43:07 +0100

    standard single bench shapes

--- src/main/java/com/mcmoddev/ironagefurniture/api/Blocks/Bench.java
+++ src/main/java/com/mcmoddev/ironagefurniture/api/Blocks/Bench.java
@@ -1,12 +1,21 @@
 package com.mcmoddev.ironagefurniture.api.Blocks;
 
-import com.mcmoddev.ironagefurniture.api.Enumerations.BenchType;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.mcmoddev.ironagefurniture.api.entity.Seat;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Direction;
-import net.minecraftforge.common.ToolType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.IBooleanFunction;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.World;
 
 public class Bench extends BackBench {
 
@@ -14,27 +23,35 @@ public class Bench extends BackBench {
 		super(hardness, blastResistance, sound, name);
 	}
 	
-	
-//	public Bench(Material materialIn, String name, float resistance, boolean tall, double yOffset, float hardness) {
-//		super(materialIn, name, resistance, tall, yOffset, hardness);
-//		// TODO Auto-generated constructor stub
-//	}
-//
+	 @Override
+    public boolean onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult rayTraceResult)
+    {
+        return Seat.create(world, pos, 0.2, player);
+    }
+	 
+	 @Override
+	protected void generateShapes(ImmutableList<BlockState> states)
+    {
+        ImmutableMap.Builder<BlockState, VoxelShape> builder = new ImmutableMap.Builder<>();
+        for(BlockState state : states)
+        {
+        	VoxelShape shapes = VoxelShapes.empty();
+        
+    		// bench body
+        	shapes = VoxelShapes.combine(shapes, getShapes(rotate(Block.makeCuboidShape(0, 6, 1, 16, 7, 15), Direction.SOUTH))[state.get(DIRECTION).getHorizontalIndex()], IBooleanFunction.OR); // chair base
+        	
+        	//bench leg
+        	shapes = VoxelShapes.combine(shapes, getShapes(rotate(Block.makeCuboidShape(2, 0, 4, 3, 7, 12), Direction.SOUTH))[state.get(DIRECTION).getHorizontalIndex()], IBooleanFunction.OR); 
+        	shapes = VoxelShapes.combine(shapes, getShapes(rotate(Block.makeCuboidShape(13, 0, 4, 14, 7, 12), Direction.SOUTH))[state.get(DIRECTION).getHorizontalIndex()], IBooleanFunction.OR);
+        	
+        	//bench cross bar
+        	shapes = VoxelShapes.combine(shapes, getShapes(rotate(Block.makeCuboidShape(0, 2, 7, 16, 4, 9), Direction.SOUTH))[state.get(DIRECTION).getHorizontalIndex()], IBooleanFunction.OR); 
+             	
+        	
+            builder.put(state, shapes.simplify());
+        }
+        
+        _shapes = builder.build();
+    }
 //	private static final AxisAlignedBB BBSHORT = new AxisAlignedBB(0.2, 0.0, 0.2, 0.9, 0.45, 0.9);
-//	
-//		
-//	@Override
-//	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) 
-//	{
-//		return BBSHORT;
-//	}
-//	
-//	@Override
-//	public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox,
-//			List<AxisAlignedBB> collidingBoxes, Entity entityIn) {
-//		
-//		if (!(entityIn instanceof Seat)) {
-//			super.addCollisionBoxToList(pos, entityBox, collidingBoxes, BBSHORT);
-//		}
-//	}
 }
