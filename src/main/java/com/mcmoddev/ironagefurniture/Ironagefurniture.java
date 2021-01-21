3ea2ac4f69a795f86bf2936291720739db278dd8
SkyBlade1978 <johnbraham1978@outlook.com>
2021-01-21 19:54:45 +0100

    chair model updates

--- src/main/java/com/mcmoddev/ironagefurniture/Ironagefurniture.java
+++ src/main/java/com/mcmoddev/ironagefurniture/Ironagefurniture.java
@@ -22,7 +22,7 @@ import net.minecraftforge.fml.common.registry.EntityRegistry;
 public class Ironagefurniture
 {
     public static final String MODID = "ironagefurniture";
-    public static final String VERSION = "0.2.0.4";
+    public static final String VERSION = "0.2.0.5";
     
 	public static final Map<String,Block> BlockRegistry = new HashMap<String, Block>();
 	public static final Map<String,Item> ItemRegistry = new HashMap<String, Item>();
