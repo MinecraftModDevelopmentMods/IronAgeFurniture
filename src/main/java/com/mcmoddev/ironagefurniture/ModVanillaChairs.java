2331f0d01dae4304e0bde8dcecc9dd21f48c7fdf
JohnBraham <johnbraham1978@outlook.com>
2025-05-13 16:34:54 +0100

    chairs registered with correct properties

--- src/main/java/com/mcmoddev/ironagefurniture/ModVanillaChairs.java
+++ src/main/java/com/mcmoddev/ironagefurniture/ModVanillaChairs.java
@@ -3,30 +3,24 @@ package com.mcmoddev.ironagefurniture;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
-import net.minecraft.world.level.material.MaterialColor;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-import javax.annotation.Nullable;
-
 import com.mcmoddev.ironagefurniture.api.blocks.furniture.Chair;
 
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class ModVanillaChairs {
 	public static final DeferredRegister<Block> REGISTER = DeferredRegister.create(ForgeRegistries.BLOCKS, Ironagefurniture.MODID);
 		
-	public static RegistryObject<Block> chair_wood_ironage_classic_oak = register("chair_wood_ironage_classic_oak", () -> new Chair(BlockBehaviour.Properties.of(Material.WOOD)));
-	public static RegistryObject<Block> chair_wood_ironage_classic_acacia = register("chair_wood_ironage_classic_acacia", () -> new Chair(BlockBehaviour.Properties.of(Material.WOOD)));
-	public static RegistryObject<Block> chair_wood_ironage_classic_dark_oak = register("chair_wood_ironage_classic_dark_oak", () -> new Chair(BlockBehaviour.Properties.of(Material.WOOD)));
-	public static RegistryObject<Block> chair_wood_ironage_classic_birch = register("chair_wood_ironage_classic_birch", () -> new Chair(BlockBehaviour.Properties.of(Material.WOOD)));
-	public static RegistryObject<Block> chair_wood_ironage_classic_jungle = register("chair_wood_ironage_classic_jungle", () -> new Chair(BlockBehaviour.Properties.of(Material.WOOD)));
-	public static RegistryObject<Block> chair_wood_ironage_classic_spruce = register("chair_wood_ironage_classic_spruce", () -> new Chair(BlockBehaviour.Properties.of(Material.WOOD)));
+	public static RegistryObject<Block> chair_wood_ironage_classic_oak = register("chair_wood_ironage_classic_oak", () -> new Chair(1, 10, SoundType.WOOD, "chair_wood_ironage_classic_oak"));
+	public static RegistryObject<Block> chair_wood_ironage_classic_acacia = register("chair_wood_ironage_classic_acacia", () -> new Chair(1, 10, SoundType.WOOD, "chair_wood_ironage_classic_acacia"));
+	public static RegistryObject<Block> chair_wood_ironage_classic_dark_oak = register("chair_wood_ironage_classic_dark_oak", () -> new Chair(1, 10, SoundType.WOOD, "chair_wood_ironage_classic_dark_oak"));
+	public static RegistryObject<Block> chair_wood_ironage_classic_birch = register("chair_wood_ironage_classic_birch", () -> new Chair(1, 10, SoundType.WOOD, "chair_wood_ironage_classic_birch"));
+	public static RegistryObject<Block> chair_wood_ironage_classic_jungle = register("chair_wood_ironage_classic_jungle", () -> new Chair(1, 10, SoundType.WOOD, "chair_wood_ironage_classic_jungle"));
+	public static RegistryObject<Block> chair_wood_ironage_classic_spruce = register("chair_wood_ironage_classic_spruce", () -> new Chair(1, 10, SoundType.WOOD, "chair_wood_ironage_classic_spruce"));
 	
 	 private static RegistryObject<Block> register(String name, Supplier<Block> block)
 	    {
@@ -40,18 +34,18 @@ public class ModVanillaChairs {
 	        return registryObject;
 	    }
 
-	    private static RegistryObject<Block> register(String name, Supplier<Block> block, @Nullable Function<RegistryObject<Block>, BlockItem> function)
-	    {
-	        RegistryObject<Block> registryObject = REGISTER.register(name, block);
-	        if(function != null)
-	        {
-	            ModItems.REGISTER.register(name, () -> function.apply(registryObject));
-	        }
-	        return registryObject;
-	    }
-
-	    private static RegistryObject<Block> registerNoItem(String name, Supplier<Block> block)
-	    {
-	        return REGISTER.register(name, block);
-	    }
+//	    private static RegistryObject<Block> register(String name, Supplier<Block> block, @Nullable Function<RegistryObject<Block>, BlockItem> function)
+//	    {
+//	        RegistryObject<Block> registryObject = REGISTER.register(name, block);
+//	        if(function != null)
+//	        {
+//	            ModItems.REGISTER.register(name, () -> function.apply(registryObject));
+//	        }
+//	        return registryObject;
+//	    }
+//
+//	    private static RegistryObject<Block> registerNoItem(String name, Supplier<Block> block)
+//	    {
+//	        return REGISTER.register(name, block);
+//	    }
 }
