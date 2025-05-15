6cf8a26e54a88fd72602802cfd4533ccabf1c76e
JohnBraham <johnbraham1978@outlook.com>
2025-05-15 12:04:04 +0100

    BoP Integration working

--- src/main/java/com/mcmoddev/ironagefurniture/Ironagefurniture.java
+++ src/main/java/com/mcmoddev/ironagefurniture/Ironagefurniture.java
@@ -3,12 +3,14 @@ package com.mcmoddev.ironagefurniture;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import com.mcmoddev.ironagefurniture.api.entity.Entities;
 import com.mcmoddev.ironagefurniture.client.renderer.ClientHandler;
+import com.mcmoddev.ironagefurniture.init.ModBOPBlocks;
 import com.mcmoddev.ironagefurniture.init.ModVanillaBackBench;
 import com.mcmoddev.ironagefurniture.init.ModVanillaBench;
 import com.mcmoddev.ironagefurniture.init.ModVanillaChairs;
@@ -32,7 +34,7 @@ import org.slf4j.Logger;
 public class Ironagefurniture
 {
     public static final String MODID = "ironagefurniture";
-    public static final String VERSION = "0.3.0.0";
+    public static final String VERSION = "0.3.0.3";
     public static final CommonProxy PROXY = DistExecutor.runForDist(() -> com.mcmoddev.ironagefurniture.proxy.ClientProxy::new, () -> CommonProxy::new);
     private static final Logger LOGGER = LogUtils.getLogger();
     
@@ -51,6 +53,12 @@ public class Ironagefurniture
 		ModVanillaLogBench.REGISTER.register(modEventBus);
 		ModVanillaPaddedBench.REGISTER.register(modEventBus);
 		ModVanillaPaddedBackBench.REGISTER.register(modEventBus);
+		
+		if (ModList.get().isLoaded("biomesoplenty")) {
+			LOGGER.info("Iron Age Furniture Biomes O Plenty Integration is loading...");
+			ModBOPBlocks.REGISTER.register(modEventBus);
+		}
+			
 		
         ModItems.REGISTER.register(modEventBus);
         Entities.REGISTER.register(modEventBus);
